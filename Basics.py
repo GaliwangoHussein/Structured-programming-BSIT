3bbf906ea28beab315ab19641fd36ff41c010cdb
Galiwango Hussein <169041230+GaliwangoHussein@users.noreply.github.com>
2025-06-04 13:35:17 +0300

    practice

---
+++ Basics.py
@@ -0,0 +1,18 @@
+# <- single line comment
+"""
+Multi line comment
+
+this is another commented line
+"""
+
+# print("this is my name") ctrl + /
+
+# python coding case (snake case)
+"""
+first_name
+galiwango hussein
+"""
+
+# Indent
+first_name = "hussein"
+print(first_name)
---
+++ variables.py
@@ -0,0 +1,28 @@
+### Assignment 1: Differentiate between Assigning
+
+
+## value
+
+## data type
+"""
+integers
+floats
+strings/characters
+Boolean
+"""
+number_one = 30 # int
+number_two = 3.17 # float
+
+first_name = "galiwango"
+last_name = "hussein"
+is_attentive = False
+is_present = True
+
+## checking for the datatype
+print(type(number_one))
+
+## conversions
+new_number_two = int(number_two)
+print(f"integer: {new_number_two} datatype: {type(number_two)}")
+
+age = "8" ##
