b7185cfcc14f81c05a921cdb482c5d39c7fd1b8c
Galiwango Hussein <169041230+GaliwangoHussein@users.noreply.github.com>
2025-06-05 13:37:21 +0300

    Add files via upload

---
+++ msql practice.sql
@@ -0,0 +1,15 @@
+create database bsit;
+show databases;
+use bsit;
+create table students(
+id INT PRIMARY KEY,
+first_name VARCHAR (25),
+last_name VARCHAR (20),
+age INT);
+DESC students;
+show tables;
+INSERT INTO students VALUES (
+1,"Tonny","Kisomose",10)
+INSERT INTO students VALUES (
+2, "KEJJE", "CHAUNDRA",32);
+INSERT INTO students
